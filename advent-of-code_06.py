b732ebad3a2ea79dc2f9141b0539eea76ac7db1f
Rodrigo Casado Noguerales <rcasado@Rodrigos-MacBook-Pro-2.local>
2024-01-14 15:32:39 +0100

    changed names

--- advent-of-code_06.py
+++ advent-of-code_06.py
@@ -1,10 +1,5 @@
 from math import sqrt, ceil, floor
 
-data = """
-Time:        40     81     77     72
-Distance:   219   1012   1365   1089
-"""
-
 # data = """
 # Time:      7  15   30
 # Distance:  9  40  200
